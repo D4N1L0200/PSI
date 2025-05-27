306504dd0d0646c60a01099b1b8a1536231330bb
D4N1L0200 <danilodantas7002@gmail.com>
2025-05-27 15:44:48 -0300

    Added more templates

--- store/urls/home_urls.py
+++ store/urls/home_urls.py
@@ -1,4 +1,5 @@
 from django.urls import path  # type: ignore
+from django.shortcuts import redirect  # type: ignore
 from store.views.home_view import home_view
 
 urlpatterns = [
--- store/views/home_view.py
+++ store/views/home_view.py
@@ -1,20 +1,5 @@
-from django.http import HttpResponse  # type: ignore
+from django.shortcuts import render  # type: ignore
 
 
-def home_view(request) -> HttpResponse:
-    links: list[str] = [
-        "<a href='/products/'>List products</a>",
-        "<a href='/extra/emoji/'>Emoji</a>",
-        "<a href='/extra/snake/'>Snake</a>",
-        "<a href='/extra/about/'>About</a>",
-    ]
-
-    html: str = """<h1>Hello, InfoWeb!</h1>
-    <ul>
-    """
-    for link in links:
-        html += f"<li>{link}</li>"
-
-    html += "</ul>"
-
-    return HttpResponse(html)
+def home_view(request):
+    return render(request, template_name="home/home.html", status=200)
